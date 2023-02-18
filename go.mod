838f5dea64fb29e8c6871b417c2461d80a77f9d6
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-02-18 13:43:23 +0530

    INITIAL COMMIT: Add ping request

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/crosine/ganymede
+
+go 1.19
---
+++ models/base/ping.go
@@ -0,0 +1,51 @@
+package base
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type Ping struct {
+	_msgpack     struct{} `msgpack:",as_array"`
+	Architecture string   `msgpack:"arch"`
+	OS           string   `msgpack:"os"`
+}
+
+func NewPing() *Ping {
+	return NewPingFromArgs(runtime.GOARCH, runtime.GOOS)
+}
+
+func NewPingFromArgs(arch string, os string) *Ping {
+	return &Ping{
+		Architecture: arch,
+		OS:           os,
+	}
+}
+
+func ConvertFromInterface(args interface{}) (*Ping, error) {
+	var ping *Ping
+	// We're now focusing on array-based MessagePack only (CASE 2). Remove it if
+	//  we're sticking with array-based serialization.
+	switch convertVal := args.(type) {
+	case map[string]interface{}:
+		ping = NewPingFromArgs(convertVal["os"].(string), convertVal["arch"].(string))
+	case []interface{}:
+		ping = NewPingFromArgs(convertVal[0].(string), convertVal[1].(string))
+	default:
+		return nil, fmt.Errorf("cannot convert using interface")
+	}
+	return ping, nil
+}
+
+// TODO: Chances are this will be integrated into server.
+// func ProcessPing(args interface{}) {
+// 	pingVal, convertErr := ConvertFromInterface(args)
+// 	if convertErr != nil {
+// 		log.Fatal(convertErr)
+// 	}
+// 	fmt.Println(pingVal.String())
+// }
+
+func (p *Ping) String() string {
+	return fmt.Sprintf("PING <ARCH: %s, OS: %s>", p.Architecture, p.OS)
+}
