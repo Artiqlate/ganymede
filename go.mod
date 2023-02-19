f111c53714b5ab3a8d1ec50b98e8ee048bf9c23c
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-02-19 21:12:25 +0530

    feat: Add MPRIS parsing

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/CrosineEnterprises/ganymede
 go 1.19
 
 require (
+	github.com/godbus/dbus/v5 v5.1.0
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 )
--- models/base/mediaplayer_metadata.go
+++ models/base/mediaplayer_metadata.go
@@ -1,11 +1,75 @@
+/*
+Media Player Metadata
+REFERENCE: https://www.freedesktop.org/wiki/Specifications/mpris-spec/metadata/
+*/
 package base
+
+import (
+	"fmt"
+
+	"github.com/godbus/dbus/v5"
+)
+
+const (
+	TRACKID = "xesam:trackid"
+	LENGTH  = "xesam:trackid"
+	TITLE   = "xesam:title"
+	ARTIST  = "xesam:artist"
+	// ALBUM   = "xesam:album"
+	// ALBUM_ARTIST = "xesam:albumArtist"
+)
 
 type MediaPlayerMetadata struct {
 	_msgpack struct{} `msgpack:",as_array"`
+	TrackId  string   `msgpack:"trackid"`
+	Length   int64    `msgpack:"length"`
 	Title    string   `msgpack:"title"`
-	Artist   string   `msgpack:"artist"`
+	Artist   []string `msgpack:"artist"`
 }
 
 func NewMediaPlayerMetadata(title string, artist string) *MediaPlayerMetadata {
 	return &MediaPlayerMetadata{}
+}
+
+func StoreMetadataValue(metadata map[string]dbus.Variant, key string, value interface{}) error {
+	metadataParseErr := metadata[key].Store(value)
+	if metadataParseErr != nil {
+		return metadataParseErr
+	}
+	return nil
+}
+
+func MediaPlayerFromMpris(metadata map[string]dbus.Variant) *MediaPlayerMetadata {
+	var trackId string
+	var length int64
+	var title string
+	var artist []string
+	// We have to do this individually because of Go's constraints
+	// trackid
+	metaErr := StoreMetadataValue(metadata, TRACKID, &trackId)
+	if metaErr != nil {
+		fmt.Println(metaErr)
+		metaErr = nil
+	}
+	metaErr = StoreMetadataValue(metadata, LENGTH, &length)
+	if metaErr != nil {
+		fmt.Println(metaErr)
+		metaErr = nil
+	}
+	metaErr = StoreMetadataValue(metadata, TITLE, &title)
+	if metaErr != nil {
+		fmt.Println(metaErr)
+		metaErr = nil
+	}
+	metaErr = StoreMetadataValue(metadata, ARTIST, &artist)
+	if metaErr != nil {
+		fmt.Println(metaErr)
+	}
+
+	return &MediaPlayerMetadata{
+		TrackId: trackId,
+		Length:  length,
+		Title:   title,
+		Artist:  artist,
+	}
 }
