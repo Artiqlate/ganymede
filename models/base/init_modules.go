92a13b82e665bac0d99d321ec8bd88c24ec6e99f
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-02-25 22:37:00 +0530

    feat: Add InitModules

--- models/base/init_modules.go
+++ models/base/init_modules.go
@@ -4,3 +4,9 @@ type InitModules struct {
 	_msgpack       struct{} `msgpack:",as_array"`
 	EnabledModules []string `msgpack:"enabledModules"`
 }
+
+func NewInitModules(enabledModules []string) *InitModules {
+	return &InitModules{
+		EnabledModules: enabledModules,
+	}
+}
