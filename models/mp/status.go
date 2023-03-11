86480452e1851d473fbd6bf5a1088302dc380b86
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-03-11 21:58:23 +0530

    feat!: Remove status struct

--- models/mp/status.go
+++ models/mp/status.go
@@ -1,11 +1,5 @@
 package mp
 
-type MPlayerStatus struct {
-	_msgpack    struct{} `msgpack:",as_array"`
-	PlayerIndex int
-	PlayStatus  string
-}
-
 type Status struct {
 	_msgpack struct{}            `msgpack:",as_array"`
 	Status   string              `msgpack:"status"`
