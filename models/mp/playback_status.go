541eaf33b178aa54bef9959ef502d5f29a5aebfe
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-03-23 12:27:08 +0530

    feat: Add mediap player playback status

---
+++ models/mp/playback_status.go
@@ -0,0 +1,20 @@
+package mp
+
+const (
+	// -- PLAYBACK STATUS --
+	// Support
+	// - Windows
+	// - Linux
+	// - macOS (* needs to be built as proxy using AVPlayer.rate == 0)
+	PlaybackStatusPlaying = "Playing"
+	PlaybackStatusPaused  = "Paused"
+	// Support
+	// - Windows
+	// - Linux
+	PlaybackStatusStopped = "Stopped"
+	// Support
+	// - Windows
+	PlaybackStatusChanging = "Changing"
+	PlaybackStatusClosed   = "Closed"
+	PlaybackStatusError    = "PlaybackStatusError"
+)
