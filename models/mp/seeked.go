f1b58066cca8f0ec0ffc01647f659d81ff26684a
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-03-24 16:50:20 +0530

    chore: fix linting issues

--- models/mp/seeked.go
+++ models/mp/seeked.go
@@ -1,6 +1,7 @@
 package mp
 
 type Seeked struct {
+	//lint:ignore U1000 `msgpack` options, not for serialization.
 	_msgpack   struct{} `msgpack:",as_array"`
 	PlayerName string   `msgpack:"playerName"`
 	PlayerIdx  int      `msgpack:"playerIdx"`
