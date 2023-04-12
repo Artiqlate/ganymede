ee97cb9c2c442459340e20dd0841109d65510261
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-04-12 08:13:36 +0530

    refactor!: Changed order for Seeked schema

--- models/mp/signals/seeked.go
+++ models/mp/signals/seeked.go
@@ -2,9 +2,9 @@ package mp_signals
 
 type Seeked struct {
 	//lint:ignore U1000 `msgpack` options, not for serialization.
-	_msgpack   struct{} `msgpack:",as_array"`
-	PlayerName string   `msgpack:"playerName"`
-	PlayerIdx  int      `msgpack:"playerIdx"`
+	_msgpack    struct{} `msgpack:",as_array"`
+	PlayerIndex int      `msgpack:"playerIdx"`
+	PlayerName  string   `msgpack:"playerName"`
 	// "seeked" time in microseconds (us).
 	SeekedInUs int64 `msgpack:"seekedInUs"`
 }
